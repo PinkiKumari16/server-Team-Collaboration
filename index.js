98db5642db04a5c46c16fc058483003ca5538dd2
PinkiKumari16 <pinkikmr2672002@gmail.com>
2025-06-08 19:26:02 +0530

    add the frontend url in evey page

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://client-team-collaboration-7jes.vercel.app",
+    origin: "https://client-team-collaboration.vercel.app",
     methods: ["GET", "POST"],
   },
 });
